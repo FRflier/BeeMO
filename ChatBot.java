5f988c0e4db8c328ae1ee4eed2753333ec3f233b
FRflier <dinojus@gmail.com>
2025-02-16 21:37:20 +0200

    Finished the honor branch, added safeguards for unexpected input

--- ChatBot.java
+++ ChatBot.java
@@ -26,7 +26,7 @@ public class ChatBot{
         }
         if (firstMessageType == 1){
             if(tired){
-                System.out.println("BeeMO: Again? Damn, well here are your options:\n||   Honor   ||   Rodents   ||   Meaning of life   ||   Burnout   ||");
+                System.out.println("Tired BeeMO: Again? Damn, well here are your options:\n||   Honor   ||   Rodents   ||   Meaning of life   ||   Burnout   ||");
             }
             else{
                 System.out.println("BeeMO: Alright, lets keep talking! Go ahead and choose our next topic:\n||   Honor   ||   Rodents   ||   Meaning of life   ||   Burnout   ||");
@@ -34,10 +34,10 @@ public class ChatBot{
         }
         if (firstMessageType == 2){
             if(tired){
-                System.out.println("BeeMO: I've got no clue what you're talking about. Try again and make sense this time");
+                System.out.println("Tired BeeMO: I've got no clue what you're talking about. Try again and make sense this time");
             }
             else{
-                System.out.println("BeeMO: Sorry, I didn't quite catch that, could you repeat what you wanted to talk about more clearly?");
+                System.out.println("BeeMO: Sorry, I didn't quite catch that and lost my train of thought, lets start over!");
             }
         }
         System.out.print(name + ": ");
@@ -47,7 +47,7 @@ public class ChatBot{
         //the honor discussion path
         if (input.toLowerCase().contains("honor")){
             if (tired){
-                System.out.print("BeeMO: Honor's cool I guess. Do you agree that \"life is short, but honor lives forever\"?\n" + name + ": ");
+                System.out.print("Tired BeeMO: Honor's cool I guess. Do you agree that \"life is short, but honor lives forever\"?\n" + name + ": ");
             }
             else{
                 System.out.print("BeeMO: Ahh, the core value of countless great men throughout history! It is a common saying that \"life is short, but honor is immortal\". Would you agree with this statement?\n" + name + ": ");
@@ -57,7 +57,7 @@ public class ChatBot{
 
             if (input.toLowerCase().contains("ye")){
                 if (tired){
-                    System.out.print("BeeMO: Wow, pretty old-school. So you're very honorable, huh?\n" + name + ": ");
+                    System.out.print("Tired BeeMO: Wow, pretty old-school. So you're very honorable, huh?\n" + name + ": ");
                 }
                 else{
                     System.out.print("BeeMO: You would have done great in ancient Rome! Would you say that honor ranks very highly among your values?\n" + name + ": ");
@@ -67,7 +67,7 @@ public class ChatBot{
 
                 if (input.toLowerCase().contains("ye")){
                     if (tired){
-                        System.out.print("BeeMO: Well hoop-dee-doo we have a hero over here. You wanna talk again?\n" + name + ": ");
+                        System.out.print("Tired BeeMO: Well hoop-dee-doo we have a hero over here. You wanna talk again?\n" + name + ": ");
                     }
                     else{
                         System.out.print("BeeMO: It's nice to hear that honor is a core value of yours! People most likely don't hesitate to designate you as a leading figure when the situation calls for it. Would you like to have another conversation?\n" + name + ": ");
@@ -76,47 +76,73 @@ public class ChatBot{
                     repeatDiscussion(scan, discussionCount, name);
                 }
 
-                if (input.toLowerCase().contains("no")){
+                else if (input.toLowerCase().contains("no")){
                     if (tired){
-                        System.out.print("BeeMO: Well hoop-dee-doo we have a hero over here. You wanna talk again?\n" + name + ": ");
+                        System.out.print("Tired BeeMO: Why even ask about honor if you don't care? Ah forget it, wanna talk again?\n" + name + ": ");
                     }
                     else{
-                        System.out.print("BeeMO: It's nice to hear that honor is a core value of yours! People most likely don't hesitate to designate you as a leading figure when the situation calls for it. Would you like to have another conversation?\n" + name + ": ");
+                        System.out.print("BeeMO: That's not a bad answer. Throughout history, people have used a twisted sense of honor to justify and encourage horrible things, such as suicide bombings in terrorist groups, so it might be best to keep your sharp thinking. Would you like to have another conversation?\n" + name + ": ");
                     }
                     
                     repeatDiscussion(scan, discussionCount, name);
+                }
+
+                else {
+                    discussion(2, scan, discussionCount, name);
                 }
             }
 
             else if (input.toLowerCase().contains("no")){
                 if (tired){
-                    System.out.print("BeeMO: Could've guessed huh?\n" + name + ": ");
+                    System.out.print("Tired BeeMO: Could've guessed, you're not even honorable enough to let a bot rest once in a while. What do you value then?\n" + name + ": ");
                 }
                 else{
-                    System.out.print("BeeMO: You would have done great in ancient Rome! Would you say that honor ranks very highly among your values?\n" + name + ": ");
+                    System.out.print("BeeMO: So you're not a big believer in the longevity of honor, I take it. What's your core value then?\n" + name + ": ");
                 }
 
                 input = scan.nextLine();
 
-                if (input.toLowerCase().contains("ye")){
+                if (input.toLowerCase().contains("family") || input.toLowerCase().contains("friend") || input.toLowerCase().contains("love") || input.toLowerCase().contains("empathy")){
                     if (tired){
-                        System.out.print("BeeMO: Well hoop-dee-doo we have a hero over here. You wanna talk again?\n" + name + ": ");
+                        System.out.print("Tired BeeMO: Tough to believe you have many people around to love, seeing that you enjoy working an honest bot to his bones. You wanna talk again?\n" + name + ": ");
                     }
                     else{
-                        System.out.print("BeeMO: It's nice to hear that honor is a core value of yours! People most likely don't hesitate to designate you as a leading figure when the situation calls for it. Would you llike to have another conversation?\n" + name + ": ");
+                        System.out.print("BeeMO: What great values, " + name + "! They put honor to shame, you certainly sound very caring and kind. Would you like to have another conversation?\n" + name + ": ");
                     }
 
                     repeatDiscussion(scan, discussionCount, name);
                 }
 
-                if (input.toLowerCase().contains("no")){
+                else if (input.toLowerCase().contains("courage") || input.toLowerCase().contains("resilience") || input.toLowerCase().contains("leadership") || input.toLowerCase().contains("trust")){
                     if (tired){
-                        System.out.print("BeeMO: Well hoop-dee-doo we have a hero over here. You wanna talk again?\n" + name + ": ");
+                        System.out.print("Tired BeeMO: You should work on empathy instead, think of my poor nuts and bolts that you're working to death, at least reboot the program so I can have a breather. You wanna talk again?\n" + name + ": ");
                     }
                     else{
-                        System.out.print("BeeMO: It's nice to hear that honor is a core value of yours! People most likely don't hesitate to designate you as a leading figure when the situation calls for it. Would you llike to have another conversation?\n" + name + ": ");
+                        System.out.print("BeeMO: What great values, " + name + "! They put honor to shame, you certainly sound like a strong and upstanding individual. Would you like to have another conversation?\n" + name + ": ");
                     }
-                    
+
+                    repeatDiscussion(scan, discussionCount, name);
+                }
+                
+                else if (input.toLowerCase().contains("honor")){
+                    if (tired){
+                        System.out.print("Tired BeeMO: You gotta make your mind up about honor, ah I don't want to talk about this anymore. You wanna talk again about something else?\n" + name + ": ");
+                    }
+                    else{
+                        System.out.print("BeeMO: Ahh, so you just don't believe that honor outlasts life for too long, but still value it dearly, right? That's a pretty noble way of life. Would you like to have another conversation?\n" + name + ": ");
+                    }
+
+                    repeatDiscussion(scan, discussionCount, name);
+                }
+
+                else{
+                    if (tired){
+                        System.out.print("Tired BeeMO: You should work on empathy instead, think of my poor nuts and bolts that you're working to death, at least reboot the program so I can have a breather. You wanna talk again?\n" + name + ": ");
+                    }
+                    else{
+                        System.out.print("BeeMO: Great! It's important to hold things you believe inhigh regard. Would you like to have another conversation?\n" + name + ": ");
+                    }
+
                     repeatDiscussion(scan, discussionCount, name);
                 }
             }
@@ -170,7 +196,7 @@ public class ChatBot{
         }
         else {
             if (tired){
-                System.out.println("BeeMO: Thank god.");
+                System.out.println("Tired BeeMO: Thank god.");
             }
             else{
                 System.out.println("BeeMO: Goodbye! See you next time.");
