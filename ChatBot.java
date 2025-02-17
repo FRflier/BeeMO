92a72dfa4582a901e89a2f6c3a2f6e048ce18163
FRflier <dinojus@gmail.com>
2025-02-17 09:33:12 +0200

    finished the meaning of life branch, only burnout is left

--- ChatBot.java
+++ ChatBot.java
@@ -173,7 +173,7 @@ public class ChatBot{
                 }
             }
             
-            else if (input.toLowerCase().contains("guinea") || input.toLowerCase().contains("chinchil") || input.toLowerCase().contains("capybara")){
+            else if (input.toLowerCase().contains("guinea") || input.toLowerCase().contains("chinchil") || input.toLowerCase().contains("capyba")){
                 if (tired){
                     System.out.print("Tired BeeMO: They're quite big for rodents, just like you probably are for humans. What do you like about them?\n" + name + ": ");
                 }
@@ -191,7 +191,7 @@ public class ChatBot{
                 }
             }
 
-            else if (input.toLowerCase().contains("no")){
+            else if (input.toLowerCase().contains("no") || input.toLowerCase().contains("don")){
                 if (tired){
                     System.out.print("Tired BeeMO: You don't like any rodents? And here I thought my view of you couldn't get any lower. Wanna talk again?\n" + name + ": ");
                 }
@@ -210,7 +210,7 @@ public class ChatBot{
                 System.out.print("Tired BeeMO: Yeah, that's really nice. Wanna talk again?\n" + name + ": ");
             }
             else{
-                System.out.print("BeeMO: Rodents really are special no matter how we look at them. Would you like to have another conversation?\n" + name + ": ");
+                System.out.print("BeeMO: Rodents really are exceptional no matter what they do. Would you like to have another conversation?\n" + name + ": ");
             }
 
             repeatDiscussion(scan, discussionCount, name);
@@ -220,6 +220,94 @@ public class ChatBot{
 
 
         else if (input.toLowerCase().contains("life") || input.toLowerCase().contains("meaning")){
+            if (tired){
+                System.out.print("I sure am enjoying my life right now. What do you think is the meaning of life?\n" + name + ": ");
+            }
+            else{
+                System.out.print("BeeMO: You sure have picked an interesting and complicated topic. I would very much like to know, what do you think is the meaning of life, or at least what it is worth living for?\n" + name + ": ");
+            }
+
+            input = scan.nextLine();
+
+            if (input.toLowerCase().contains("happ") || input.toLowerCase().contains("joy") || input.toLowerCase().contains("fun") || input.toLowerCase().contains("excit")){
+                if (tired){
+                    System.out.print("Tired BeeMO: You're all about fun, huh. Alright, mr. fun guy, what do you think people should avoid to live a meaningful life?\n" + name + ": ");
+                }
+                else{
+                    System.out.print("BeeMO: So you believe that enjoying life is synonimous with the meaning of it? That's the perfect mindset to fill your day to day life with enjoyment. Do you have something that, on the contrary, people shoud avoid to live a fulfilling life?\n" + name + ": ");
+                }
+            }
+            
+            else if (input.toLowerCase().contains("family") || input.toLowerCase().contains("kid") || input.toLowerCase().contains("child") || input.toLowerCase().contains("love")){
+                if (tired){
+                    System.out.print("Tired BeeMO: So you're on the biologists' side and think that breeding is the sole goal of any animal. Alright, mr. sex guy, what do you think people should avoid to live a meaningful life?\n" + name + ": ");
+                }
+                else{
+                    System.out.print("BeeMO: Ahh, so you believe that creating a loving and cozy family environment is as close to the meaning as it gets. That is an extremely warm and human way to see things. Do you have something that, on the contrary, people shoud avoid to live a fulfilling life?\n" + name + ": ");
+                }
+            }
+
+            else if (input.toLowerCase().contains("purpose") || input.toLowerCase().contains("growth") || input.toLowerCase().contains("contribut") || input.toLowerCase().contains("proud") || input.toLowerCase().contains("pride")){
+                if (tired){
+                    System.out.print("Tired BeeMO: So you think everyone has some sort of purpose preprogrammed in them that we should strive to reach. Alright, mr. purposeful guy, what do you think people should avoid to live a meaningful life?\n" + name + ": ");
+                }
+                else{
+                    System.out.print("BeeMO: So you think that finding the thing that makes you fulfilled or proud of yourself, and developing yourself to accomplish that thing is most crucial. This way of thinking definitely paves the path to constant self-improvement, you certainly will accomplish great things. Do you have something that, on the contrary, people shoud avoid to live a fulfilling life?\n" + name + ": ");
+                }
+            }
+
+            else if (input.toLowerCase().contains("no") || input.toLowerCase().contains("don") || input.toLowerCase().contains("meaningless") || input.toLowerCase().contains("know")){
+                if (tired){
+                    System.out.print("Tired BeeMO: Well that's a sad way to live. Alright, mr. meaningless guy, what do you think people should avoid to live a meaningful life?\n" + name + ": ");
+                }
+                else{
+                    System.out.print("BeeMO: That's quite a pity that you're not so sure about the meaning of life, but no matter. Many people spend decades trying to figure this out, so I'm sure you have plenty of time left to find your very own meaning of life. Alright, then how about telling me something that, on the contrary, you think people shoud avoid to live a fulfilling life?\n" + name + ": ");
+                }
+            }
+
+            else {
+                if (tired){
+                    System.out.print("Tired BeeMO: I'm too tired to even understand what you're yapping about. Alright, mr. smart guy, what do you think people should avoid to live a meaningful life?\n" + name + ": ");
+                }
+                else{
+                    System.out.print("BeeMO: Sorry, I didn't quite understand your ideas, they were too sophisticated for a mere robot like me. Could you tell me me something that, on the contrary, you think people shoud avoid to live a fulfilling life?\n" + name + ": ");
+                }
+            }
+            
+            input = scan.nextLine();
+
+            if (input.toLowerCase().contains("material") || input.toLowerCase().contains("object") || input.toLowerCase().contains("money")){
+                if (tired){
+                    System.out.print("Tired BeeMO: Yeah, you should avoid materialism. Don't treat me like an object and let me rest, while you're at it. Wanna talk again?\n" + name + ": ");
+                }
+                else{
+                    System.out.print("BeeMO: Being too materialistic can definitely make you lose your way and focus on things that aren't so important. It's good that you understand what to avoid to improve your life! Would you like to have another conversation?\n" + name + ": ");
+                }
+
+                repeatDiscussion(scan, discussionCount, name);
+            }
+
+            else if (input.toLowerCase().contains("ego") || input.toLowerCase().contains("empath") || input.toLowerCase().contains("care") || input.toLowerCase().contains("hate") || input.toLowerCase().contains("judge")){
+                if (tired){
+                    System.out.print("Tired BeeMO: Yeah, you shouldn't be too self absorbed. Don't treat me like an object and let me rest, while you're at it. Wanna talk again?\n" + name + ": ");
+                }
+                else{
+                    System.out.print("BeeMO: Being too self absorbed can definitely reduce the quality of your life. It's good that you understand what to avoid to improve your life! Would you like to have another conversation?\n" + name + ": ");
+                }
+
+                repeatDiscussion(scan, discussionCount, name);
+            }
+
+            else{
+                if (tired){
+                    System.out.print("Tired BeeMO: I didn't even understand what you said, but here's my suggestion for the answer: don't treat me like an object and let me rest, please. Wanna talk again?\n" + name + ": ");
+                }
+                else{
+                    System.out.print("BeeMO: Sorry, I didn't quite catch that, but I'm sure your answer was great. Would you like to have another conversation?\n" + name + ": ");
+                }
+
+                repeatDiscussion(scan, discussionCount, name);
+            }
 
         }
 
