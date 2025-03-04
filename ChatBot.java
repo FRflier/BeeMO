75a14594b40f1f2f6fa6840c61eebcd8b9d75200
FRflier <dinojus@gmail.com>
2025-03-04 14:40:49 +0200

    Way better HashMap structure, finished initialization method

--- ChatBot.java
+++ ChatBot.java
@@ -44,13 +44,14 @@ public class ChatBot {
         }
     }
     
-    // Constructor initializes dialogue data
+    // Constructor calls initialization method
     public ChatBot() {
         initializeDialogueData();
     }
     
     // Initialize all dialogue options and responses
     private void initializeDialogueData() {
+
         // Initialize topic options
         topicOptions.put("honor", new DialogueOption(
             "Ahh, the core value of countless great men throughout history! It is a common saying that \"life is short, but honor is immortal\". Would you agree with this statement?",
@@ -71,8 +72,24 @@ public class ChatBot {
             "What a coincidence, as my developer is very tired. So here's a nice and concise discussion branch for you to enjoy: Do you like bread?",
             "Great topic, as I am very tired. Just like my developer when he wrote this. So here's a simple discussion branch. Do you like bread?"
         ));
+
+
+        //honor tree
+        Map<String, DialogueResponse> honorResponses = new HashMap<>();
+        honorResponses.put("ye", new DialogueResponse(
+            "You would have done great in ancient Rome! Would you say that honor ranks very highly among your values?",
+            "Wow, pretty old-school. So you're very honorable, huh?",
+            "honor_yes"
+        ));
         
-        // Honor responses
+        honorResponses.put("no", new DialogueResponse(
+            "So you're not a big believer in the longevity of honor, I take it. What's your core value then?",
+            "Could've guessed, you're not even honorable enough to let a bot rest once in a while. What do you value then?",
+            "honor_no"
+        ));
+
+        responseTree.put("honor", honorResponses);
+        
         Map<String, DialogueResponse> honorYesResponses = new HashMap<>();
         honorYesResponses.put("ye", new DialogueResponse(
             "It's nice to hear that honor is a core value of yours! People most likely don't hesitate to designate you as a leading figure when the situation calls for it. Would you like to have another conversation?",
@@ -85,6 +102,8 @@ public class ChatBot {
             "Why even ask about honor if you don't care? Ah forget it, wanna talk again?",
             "repeat"
         ));
+
+        responseTree.put("honor_yes", honorYesResponses);
         
         Map<String, DialogueResponse> honorNoResponses = new HashMap<>();
         honorNoResponses.put("family", new DialogueResponse(
@@ -110,22 +129,11 @@ public class ChatBot {
             "You should work on empathy instead, think of my poor nuts and bolts that you're working to death, at least reboot the program so I can have a breather. You wanna talk again?",
             "repeat"
         ));
+
+        responseTree.put("honor_no", honorNoResponses);
         
-        Map<String, DialogueResponse> honorResponses = new HashMap<>();
-        honorResponses.put("ye", new DialogueResponse(
-            "You would have done great in ancient Rome! Would you say that honor ranks very highly among your values?",
-            "Wow, pretty old-school. So you're very honorable, huh?",
-            "honor_yes"
-        ));
-        
-        honorResponses.put("no", new DialogueResponse(
-            "So you're not a big believer in the longevity of honor, I take it. What's your core value then?",
-            "Could've guessed, you're not even honorable enough to let a bot rest once in a while. What do you value then?",
-            "honor_no"
-        ));
-        
-        // Add more responses for other dialogues here...
-        // Rodent responses
+
+        // Rodent tree
         Map<String, DialogueResponse> rodentTypeResponses = new HashMap<>();
         rodentTypeResponses.put("rat", new DialogueResponse(
             "Rats really are great. They're smart, cute and make great pets. Many would say that rats are the most popular rodent in the world. What's your favorite thing about rats?",
@@ -138,12 +146,109 @@ public class ChatBot {
             "They're quite big for rodents, just like you probably are for humans. What do you like about them?",
             "rodent_quality"
         ));
+
+        rodentTypeResponses.put("squir", new DialogueResponse(
+            "They're my favorite too!! Oh man, hearing that made me really happy. What's your favorite thing about them?",
+            "Oh man, I love squirrels so much that even hearing about them just brought some life back into me. What do you like about them?",
+            "rodent_quality"
+        ));
         
-        // Store the responses in the response tree
-        responseTree.put("honor", honorResponses);
-        responseTree.put("honor_yes", honorYesResponses);
-        responseTree.put("honor_no", honorNoResponses);
+        rodentTypeResponses.put("mouse", new DialogueResponse(
+            "Those tiny rodents really are great bundles of joy. What about them do you enjoy most?",
+            "You like the tiniest and cutest rodents? Do they not remind you of a certain something unpleasant? What do you like about them?",
+            "rodent_quality"
+        ));
+        
+        rodentTypeResponses.put("no", new DialogueResponse(
+            "You don't like rodents? Well to each their own, let's change the subject. Would you like to have another conversation?",
+            "You don't like any rodents? And here I thought my view of you couldn't get any lower. Wanna talk again?",
+            "repeat"
+        ));
+
         responseTree.put("rodent", rodentTypeResponses);
+
+        Map<String, DialogueResponse> rodentQualityResponses = new HashMap<>();
+        rodentQualityResponses.put("default", new DialogueResponse(
+            "That's amazing! Rodents really are exceptional no matter what they do! Would you like to have another conversation?",
+            "Yeah, that is really nice. Wanna talk again?",
+            "repeat"
+        ));
+
+        responseTree.put("rodent_quality", rodentQualityResponses);
+
+
+        // Meaning of Life tree
+        Map<String, DialogueResponse> meaningOfLifeResponses = new HashMap<>();
+        meaningOfLifeResponses.put("happy", new DialogueResponse(
+            "So you believe that enjoying life is synonymous with the meaning of it? That's the perfect mindset to fill your day to day life with enjoyment. Do you have something that, on the contrary, people should avoid to live a fulfilling life?",
+            "You're all about fun, huh. Alright, mr. fun guy, what do you think people should avoid to live a meaningful life?",
+            "meaning_avoid"
+        ));
+        
+        meaningOfLifeResponses.put("family", new DialogueResponse(
+            "Ahh, so you believe that creating a loving and cozy family environment is as close to the meaning as it gets. That is an extremely warm and human way to see things. Do you have something that, on the contrary, people should avoid to live a fulfilling life?",
+            "So you're on the biologists' side and think that breeding is the sole goal of any animal. Alright, mr. sex guy, what do you think people should avoid to live a meaningful life?",
+            "meaning_avoid"
+        ));
+        
+        meaningOfLifeResponses.put("purpose", new DialogueResponse(
+            "So you think that finding the thing that makes you fulfilled or proud of yourself, and developing yourself to accomplish that thing is most crucial. This way of thinking definitely paves the path to constant self-improvement, you certainly will accomplish great things. Do you have something that, on the contrary, people should avoid to live a fulfilling life?",
+            "So you think everyone has some sort of purpose preprogrammed in them that we should strive to reach. Alright, mr. purposeful guy, what do you think people should avoid to live a meaningful life?",
+            "meaning_avoid"
+        ));
+        
+        meaningOfLifeResponses.put("no", new DialogueResponse(
+            "That's quite a pity that you're not so sure about the meaning of life, but no matter. Many people spend decades trying to figure this out, so I'm sure you have plenty of time left to find your very own meaning of life. Alright, then how about telling me something that, on the contrary, you think people should avoid to live a fulfilling life?",
+            "Well that's a sad way to live. Alright, mr. meaningless guy, what do you think people should avoid to live a meaningful life?",
+            "meaning_avoid"
+        ));
+        
+        responseTree.put("meaning", meaningOfLifeResponses);
+
+        // Meaning of Life - Things to Avoid responses
+        Map<String, DialogueResponse> meaningAvoidResponses = new HashMap<>();
+        meaningAvoidResponses.put("material", new DialogueResponse(
+            "Being too materialistic can definitely make you lose your way and focus on things that aren't so important. It's good that you understand what to avoid to improve your life! Would you like to have another conversation?",
+            "Yeah, you should avoid materialism. Don't treat me like an object and let me rest, while you're at it. Wanna talk again?",
+            "repeat"
+        ));
+        
+        meaningAvoidResponses.put("ego", new DialogueResponse(
+            "Being too self-absorbed can definitely reduce the quality of your life. It's good that you understand what to avoid to improve your life! Would you like to have another conversation?",
+            "Yeah, you shouldn't be too self-absorbed. Don't treat me like an object and let me rest, while you're at it. Wanna talk again?",
+            "repeat"
+        ));
+        
+        meaningAvoidResponses.put("default", new DialogueResponse(
+            "Sorry, I didn't quite catch that, but I'm sure your answer was great. Would you like to have another conversation?",
+            "I didn't even understand what you said, but here's my suggestion for the answer: don't treat me like an object and let me rest, please. Wanna talk again?",
+            "repeat"
+        ));
+        
+        responseTree.put("meaning_avoid", meaningAvoidResponses);
+
+        // Exhaustion topic responses
+        Map<String, DialogueResponse> exhaustionResponses = new HashMap<>();
+        exhaustionResponses.put("yes", new DialogueResponse(
+            "That's great, {name}! Do you want to have another conversation?",
+            "Amazing. Wanna talk again?",
+            "repeat"
+        ));
+        
+        exhaustionResponses.put("no", new DialogueResponse(
+            "Well, to each their own. Do you want to have another conversation?",
+            "Damn. Wanna talk again?",
+            "repeat"
+        ));
+        
+        exhaustionResponses.put("default", new DialogueResponse(
+            "Sorry, I didn't quite understand your answer, so I'll just assume that you're a bread enthusiast. Do you want to have another conversation?",
+            "Can you not recognize a yes or no question when you see one? Oh well, Wanna talk again?",
+            "repeat"
+        ));
+        
+        responseTree.put("exhaustion", exhaustionResponses);
+        
     }
     
     // Initializes the conversation by asking for a name and creating a scanner
